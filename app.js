0be9478d655cb1511025d5d5dd1b7cf1ada0b5e9
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2022-03-17 22:01:00 -0400

    added next for frontend

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
 import 'dotenv/config';
 
 import express from 'express';
-const app = express();
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+
+const handle = app.getRequestHandler();
+const server = express();
+
+app
+	.prepare()
+	.then(() => {
+        
+		server.use((req, res, next) => {
+			// change !== to === when deployed to heroku
+			if (req.headers['x-forwarded-proto'] !== 'https') {
+				next();
+			} else {
+				res.redirect(301, `https://${req.hostname}${req.originalUrl}`);
+			}
+			
+			res.setHeader(
+				'strict-transport-security',
+				'max-age=31536000; includeSubDomains; preload'
+			);
+		});
+		
+		server.all('*', (req, res) => {return handle(req, res)});
+        
+		server.listen(process.env.PORT || 3000, (error) => {
+			if (error) throw error;
+			console.error(`Listening on port ${process.env.PORT || 3000}`);
+		});
+	})
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
 
 import chalk from 'chalk';
 
-app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ extended: true }));
+server.use(express.json({ limit: '30mb', extended: true }));
+server.use(express.urlencoded({ extended: true }));
 
 import { resolve } from 'path';
-app.use(express.static(`${resolve()}/static`, { dotfiles: 'allow' }));
+server.use(express.static(`${resolve()}/static`, { dotfiles: 'allow' }));
 
-app.enable('trust proxy');
+server.enable('trust proxy');
 
-app.listen(process.env.PORT || 3000, async (err) => {
+server.listen(process.env.PORT || 3000, async (err) => {
     if (err) {
         return console.log(chalk.red(`Error starting the server ❌\nerr`));
     }
@@ -21,12 +57,12 @@ app.listen(process.env.PORT || 3000, async (err) => {
     console.log(chalk.green('Server successfully started ✔️'));
 });
 
-app.get('/', async (req, res) => {
+server.get('/', async (req, res) => {
     return res.status(200).send('Hello, World!');
 });
 
 import oauthRoute from './routes/oauth.js';
-app.use('/oauth', oauthRoute);
+server.use('/oauth', oauthRoute);
 
 import postRoute from './routes/post.js';
-app.use('/post', postRoute);
+server.use('/post', postRoute);
