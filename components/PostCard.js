c86d00ed8e613ec35e553cdacca55e9c40365ffb
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2022-03-22 22:53:44 -0400

    added initial card design + replaced AwardStack component in favor of avatar component + removed downvotes

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,14 +1,19 @@
-import Image from 'next/image';
+import { Card, Image, Text, Group, useMantineTheme, Title, Avatar, AvatarsGroup, Center } from '@mantine/core';
+import { ArrowNarrowUp, Message2, Calendar } from 'tabler-icons-react';
 
-export default function PostCard({ id, data }) {
-    console.log(data);
+export default function PostCard({ data }) {
+    const theme = useMantineTheme();
+    
+    const secondaryColor = theme.colorScheme === 'dark'
+    ? theme.colors.dark[1]
+    : theme.colors.gray[7];
+    
     const {
         subreddit,
         title,
         body,
         author,
         upvotes,
-        downvotes,
         image,
         permalink,
         comments,
@@ -17,29 +22,51 @@ export default function PostCard({ id, data }) {
         awards
     } = data.data;
     
+    const generateAwards = (awards) => {
+        if (!awards || !awards.length) {
+            return null;
+        }
+        const topThree = awards.sort((a, b) => a - b)
+            .slice(0, 3)
+            .map(award => award.icon.url);
+        
+        const totalAwards = awards.reduce((acc, award) => acc + award.count, 0);
+        
+        return {
+            topThree,
+            totalAwards
+        };
+    };
+    
+    const { topThree, totalAwards } = generateAwards(awards);
+    
     return (
         <>
-            <ul>
-                <li>{subreddit}</li>
-                <li>{title}</li>
-                <li>{body}</li>
-                <li>{author}</li>
-                <li>{upvotes}</li>
-                <li>{downvotes}</li>
-                <li>{image}</li>
-                <li>{permalink}</li>
-                <li>{comments}</li>
-                <li>{created}</li>
-                <li>{locked}</li>
-            </ul>
-            {awards.map(award => {
-                return (
-                    <>
-                        <img src={award.icon.url} width={50} height={50} />
-                        <span>{award.count}</span>
-                    </>
-                );
-            })}
+            <div style={{ width: 400, margin: 'auto',  }} >
+                <Card shadow="sm" p="lg">
+                    <Card.Section>
+                        <Image src={image} width={400} alt={title} />
+                    </Card.Section>
+                </Card>
+                
+                <Title order={4} mt={10}>{title}</Title>
+                
+                <Text color={secondaryColor} size="md" mt={10}>in <Text color={secondaryColor} size="sm" weight="600" component="span">{subreddit} </Text>by <Text color={secondaryColor} size="sm" weight="600" component="span">u/{author}</Text></Text>
+                
+                <Group position="left">
+                    <Text weight={500} color={secondaryColor} size="sm" mt={10}><ArrowNarrowUp size={15} />{upvotes}</Text>
+                    <Text weight={500} color={secondaryColor} size="sm" mt={10}><Message2 size={15} />{comments}</Text>
+                    <Text weight={500} color={secondaryColor} size="sm" mt={10}><Calendar size={15} />{created}</Text>
+                    <AvatarsGroup size={20} limit={3} total={topThree < 3 && totalAwards}>
+                        {topThree.map(award => {
+                            return (
+                                <Avatar src={award} key={award} />
+                            );
+                        })}
+                    </AvatarsGroup>
+                    <Text color={secondaryColor} size="sm" mt={10}>{totalAwards}</Text>
+                </Group>
+            </div>
         </>
     );
 };
